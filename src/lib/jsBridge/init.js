521af4dfd29e3a030aae185ab0809824f57a0d6e
ihongtaoi <836717428@qq.com>
2019-03-07 18:33:08 +0800

    save

--- src/lib/jsBridge/init.js
+++ src/lib/jsBridge/init.js
@@ -1,14 +1,3 @@
 export default function(Vueinit) {
-  var push = api.require('push');
-  push.bind({
-      userName:'testName',
-      userId:'testId'
-  },function(ret,err){
-      if(ret.status){
-          api.alert({msg:'绑定成功'});
-      }else{
-          api.alert({msg:err.msg});
-      }
-  });
   Vueinit();
 }
--- src/page/other/login.vue
+++ src/page/other/login.vue
@@ -56,6 +56,7 @@ export default {
 </script>
 <style lang="less" scoped>
 #login {
+  background: #fff;
   header {
     text-align: center;
     padding: 20px 0;
